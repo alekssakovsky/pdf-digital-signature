7aaba74e78daeacdc9c7fa83b4ff58ed0a1f1beb
AlexKolonitsky <alex.kolonitsky@gmail.com>
2018-09-04 16:49:36 +0300

    add render link in pdf file by timer

--- application.js
+++ application.js
@@ -1,4 +1,24 @@
-let CronJob = require('cron').CronJob;
-new CronJob('0-59 * * * *', function() {
-  console.log('blah-blah-blah');
+const CronJob = require('cron').CronJob;
+const phantom = require('phantom');
+
+new CronJob('0-59 * * * *', () => {
+  renderGoogle();
 }, null, true);
+
+
+function renderGoogle() {
+  phantom.create()
+    .then((ph) => {
+      ph.createPage()
+        .then((page) => {
+          page.open("http://www.google.com")
+            .then((status) => {
+              page.render('google.pdf')
+                .then(() => {
+                  console.log('Page Rendered, status: ', status);
+                  ph.exit();
+                });
+            });
+        });
+    });
+}
