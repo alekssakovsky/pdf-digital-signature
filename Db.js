76bcf5b766b895cd03007d4b1139cee9524ec6ab
AlexKolonitsky <alex.kolonitsky@gmail.com>
2018-09-06 13:28:09 +0300

    add mysql

--- Db.js
+++ Db.js
@@ -1,12 +1,22 @@
-const SELECT_V_SITE_BY_C_ID = 'SELECT v_site FROM vendors WHERE v_id = (SELECT c_vendor_id FROM customers WHERE c_id = 1)';
+'use strict';
+const SELECT_ALL_CUSTOMERS = 'SELECT * FROM customers JOIN vendors;';
 
 const mysql = require('mysql');
 const dbConfig = require('./config/db-data');
-const connection = mysql.createConnection(dbConfig);
 
-connection.connect();
-connection.query(SELECT_V_SITE_BY_C_ID, function (err, result) {
-  if (err) throw err;
-  console.log('result: ', result[0].v_site);
-});
-connection.end();
+
+function dbConnect(query) {
+  return new Promise((resolve, reject) => {
+    const connection = mysql.createConnection(dbConfig);
+    connection.connect();
+    connection.query(query, function (err, result) {
+      if (err) reject(err);
+
+      resolve(result);
+      connection.end();
+    });
+  });
+}
+
+module.exports.dbConnect = dbConnect;
+module.exports.SELECT_ALL_CUSTOMERS = SELECT_ALL_CUSTOMERS;
--- application.js
+++ application.js
@@ -1,24 +1,59 @@
+'use strict';
 const CronJob = require('cron').CronJob;
 const phantom = require('phantom');
+const db = require('./Db');
+const SELECT_ALL_CUSTOMERS = require('./Db').SELECT_ALL_CUSTOMERS;
+let counter = 0;
 
-new CronJob('0-59 * * * *', () => {
-  renderGoogle();
+// new CronJob('0-59 * * * *', () => {
+//
+new CronJob('* * * * * *', function () {
+  counter++;
+  db.dbConnect(SELECT_ALL_CUSTOMERS)
+    .then((results) => {
+      results.forEach((result) => savePdf(result, counter));
+    });
 }, null, true);
 
 
-function renderGoogle() {
+function savePdf(data, counter) {
+  console.log(counter);
   phantom.create()
     .then((ph) => {
+      // console.log('===========================1111');
       ph.createPage()
         .then((page) => {
+          // console.log('===========================2222');
+
           page.open('https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Dinstant-video&field-keywords=Arnold')
-            .then((status) => {
-              page.render('google.pdf')
+
+            .then((response) => {
+              // console.log('===========================3333');
+
+              console.log(response);
+              page.render(counter + data.customerN + '.pdf')
                 .then(() => {
-                  console.log('Page Rendered, status: ', status);
+                  // console.log('===========================4444');
+
                   ph.exit();
                 });
             });
         });
     });
 }
+
+
+// async function savePdf(data, counter) {
+//   console.log(counter);
+//   // console.log(data.v_site + data.customerN);
+//   const instance = await phantom.create();
+//   const page = await instance.createPage();
+//
+//   // await page.open(data.v_site + data.customerN);
+//   await page.open('https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Dinstant-video&field-keywords=Arnold');
+//   console.log(page);
+//   // await page.open('http://htmlbook.ru');
+//   // await page.render(counter + 'htmlbook.pdf');
+//   await page.render(counter + data.customerN + '.pdf');
+//   await instance.exit();
+// }
