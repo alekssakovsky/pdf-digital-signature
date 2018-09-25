449fa0e122d75732e6818d8298d8356408329b22
alekssakovsky <aleks.sakovsky@gmail.com>
2018-09-25 14:38:54 +0300

    added send mail by Mailgun

--- Sender.js
+++ Sender.js
@@ -1,6 +1,10 @@
 'use strict';
-const mailer = require('nodemailer');
-const SENDER_CONFIG = require('./config/email');
+
+const MAILGUN_CONFIG = require('./config/mailgun.json');
+const mailgun = require('mailgun-js')({
+  apiKey: "2e90fe3e2f9d1aadcc773d0882c24adb-0e6e8cad-e845da62",
+  domain: "open-corp.com",
+});
 
 /**
  * Sends email.
@@ -14,11 +18,15 @@ const SENDER_CONFIG = require('./config/email');
  * @reject {string} errorStr
  */
 function sendMail(message) {
-  SENDER_CONFIG.mailOptions.text = message;
-  const transporter = mailer.createTransport(SENDER_CONFIG.mail);
+  let data = {
+    from: MAILGUN_CONFIG.SMTP_login,
+    to: MAILGUN_CONFIG.TO,
+    subject: 'link',
+    text: message
+  };
 
   return new Promise((resolve, reject) => {
-    transporter.sendMail(SENDER_CONFIG.mailOptions, (error) => {
+    mailgun.messages().send(data, function (error) {
       if (error) {
         const errorStr = `I can\'t send email: ${error}`;
         reject(errorStr);
@@ -26,8 +34,13 @@ function sendMail(message) {
         resolve();
         console.log(`Email was sent`);
       }
+
     });
   });
 }
 
 module.exports.sendMail = sendMail;
+
+sendMail('Я Вам пишу, чего же боле?')
+  .then()
+  .catch((error));
